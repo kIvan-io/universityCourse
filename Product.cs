4ad8d58362fd682a1cddf977efad0aef27a6d131
kIvan-io <ivankostyukevich54@gmail.com>
2025-11-27 19:00:59 +0300

    initial project 5

---
+++ Product.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace ProductNamespace
+{
+    public class Product
+    {
+        private string name;
+        private decimal price;
+        private int quantity;
+        private string sku;
+        private string category;
+
+        public string GetName() => name;
+        public decimal GetPrice() => price;
+        public int GetQuantity() => quantity;
+        public string GetSku() => sku;
+        public string GetCategory() => category;
+        public void SetName(string name) => this.name = name;
+        public void SetPrice(decimal price) => this.price = price;
+        public void SetQuantity(int quantity) => this.quantity = quantity;
+        public void SetCategory(string category) => this.category = category;
+
+        public Product(string name, decimal price, int quantity, string category)
+        {
+            if (ValidateInput(name, price, quantity, category) == true)
+            {
+                this.name = name;
+                this.price = price;
+                this.quantity = quantity;
+                this.category = category;
+                sku = GenerateSku();
+            }
+            else
+            {
+                this.name = "INVALID";
+                this.price = 0;
+                this.quantity = 0;
+                this.category = "INVALID";
+                sku = "INVALID";
+            }
+        }
+
+        private bool ValidateInput(string name, decimal price, int quantity, string category)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine(("Название товара не может быть пустым"));
+                return false;
+            }             
+            if (price <= 0)
+            {
+                Console.WriteLine(("Цена должна быть положительной"));
+                return false;
+            }                
+            if (quantity <= 0)
+            {
+                Console.WriteLine(("Количество не может быть отрицательным"));
+                return false;
+            }               
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                Console.WriteLine(("Категория не может быть пустой"));
+                return false;
+            }
+            return true;
+        }
+
+        private string GenerateSku()
+        {
+            return DateTime.Now.ToString("yyMMddHHmmssfff");
+        }
+
+        public void UpdatePrice(decimal newPrice)
+        {
+            if (newPrice >= 0)
+            {
+                SetPrice(newPrice);
+            }
+            else
+            {
+                Console.WriteLine("цена < 0");
+            }
+        }
+        public bool IncreaseQuantity(int amount)
+        {
+            if(amount >= 0)
+            {
+                SetQuantity(amount + GetQuantity());
+                return true;
+            }
+            return false;
+        }
+
+        public bool DecreaseQuantity(int amount)
+        {
+
+            if (amount >= 0)
+            {
+                SetQuantity(GetQuantity() - amount);
+                return true;
+            }
+            return false;
+        }
+        public void ApplyDiscount(decimal percent)
+        {
+            if (percent <= 0 || percent > 100)
+            {
+                Console.WriteLine("скидка должны быть в диапазоне от 0 до 100%");
+                return;
+            }
+            decimal discountAmount = price * (percent / 100);
+            UpdatePrice(GetPrice() - discountAmount);
+        }
+
+        public void print()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("название: " + name);
+            Console.WriteLine("цена: " + price);
+            Console.WriteLine("количество: " + quantity);
+            Console.WriteLine("артикул: " + sku);
+            Console.WriteLine("категория: " + category);
+            Console.WriteLine("==================================");
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using ProductNamespace;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Product[] products =
+            {
+                new Product("Вишня", 45, 50, "Фрукты"),
+                new Product("Морковь", 30, 100, "Овощи"),
+                new Product("Молоко", 80, 30, "Молочные продукты"),
+                new Product("Хлеб", 25, 40, "Выпечка"),
+                new Product("Сок", 120, 20, "Напитки"),
+                new Product("Вишня444", 45, -50, "Фрукты"),
+            };
+
+        //        void UpdatePrice(decimal newPrice)     ->  Изменить цену
+        //    bool IncreaseQuantity(int amount)      -> Увеличить количество
+        //    bool DecreaseQuantity(int amount)   -> Уменьшить количество
+        //void ApplyDiscount(decimal percent)    ->  Применить скидку
+
+        products[1].print();
+
+        products[1].UpdatePrice(1);
+        products[1].IncreaseQuantity(100);
+        products[1].DecreaseQuantity(2);
+        products[1].ApplyDiscount(50);
+        //products[5].
+
+
+        products[5].print();
+
+    }
+}
